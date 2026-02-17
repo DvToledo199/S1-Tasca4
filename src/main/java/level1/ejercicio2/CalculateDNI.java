b56eebe64b21ea23172dd9fc9ec2e7ef21e75c11
David Toledo <toledo_1990@hotmail.com>
2026-02-17 13:21:09 +0100

    Initialize Exercise 2 base structure

---
+++ src/main/java/level1/ejercicio2/CalculateDNI.java
@@ -0,0 +1,13 @@
+package level1.ejercicio2;
+
+public class CalculateDNI {
+
+    private int dniNumber;
+
+    public CalculateDNI(int dniNumber){
+        this.dniNumber = dniNumber;
+    }
+
+    public char calculateDniLetter
+
+}
