4db2d3741153cb05413757d73f377a66eb99f0ff
David Toledo <toledo_1990@hotmail.com>
2026-02-17 20:22:34 +0100

    Add parameterized test with 10 valid DNI cases for letter calculation

--- src/main/java/level1/ejercicio2/CalculateDNI.java
+++ src/main/java/level1/ejercicio2/CalculateDNI.java
@@ -2,12 +2,16 @@ package level1.ejercicio2;
 
 public class CalculateDNI {
 
-    private int dniNumber;
+    private static final String LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
 
-    public CalculateDNI(int dniNumber){
-        this.dniNumber = dniNumber;
+
+    public static char calculateDniLetter(int dniNumber){
+        if(dniNumber < 0 || dniNumber > 99999999){
+            throw new IllegalArgumentException("Invalid DNI numbre.");
+        }
+    int remainder = dniNumber % 23;
+    char letter = LETTERS.charAt(remainder);
+    return letter;
     }
-
-    public char calculateDniLetter
 
 }
---
+++ src/test/java/level1/ejercicio2/CalcularDNITest.java
@@ -0,0 +1,30 @@
+package level1.ejercicio2;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalcularDNITest {
+    @ParameterizedTest
+    @CsvSource({
+            "12345678, Z",
+            "00000000, T",
+            "11111111, H",
+            "22222222, J",
+            "33333333, P",
+            "44444444, A",
+            "55555555, K",
+            "66666666, Q",
+            "77777777, B",
+            "88888888, Y"
+
+    })
+    void calculateDniLetterShouldReturnCorrectLetter(int dniNumber, char expectedLetter) {
+
+        char result = CalculateDNI.calculateDniLetter(dniNumber);
+
+        assertEquals(expectedLetter, result);
+    }
+}
