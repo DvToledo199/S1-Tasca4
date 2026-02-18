a59084f697c55e603b2418c7bbd573189b11c559
David Toledo <toledo_1990@hotmail.com>
2026-02-18 19:37:16 +0100

    feat: add AssertJ exercise 6 exception assertion test

--- src/test/java/level2/ejercicio6/Exercise6Test.java
+++ src/test/java/level2/ejercicio6/Exercise6Test.java
@@ -1,4 +1,17 @@
 package level2.ejercicio6;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class Exercise6Test {
+
+    @Test
+    void shouldThrowArrayIndexOutOfBoundsException() {
+
+        assertThatThrownBy(() -> {
+            int[] numbers = new int[2];
+            int value = numbers[5];
+        }).isInstanceOf(ArrayIndexOutOfBoundsException.class);
+    }
 }
