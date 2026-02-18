adafb7bf887ea59145d716d755bafe31499f29e6
David Toledo <toledo_1990@hotmail.com>
2026-02-18 19:43:17 +0100

    feat: add AssertJ exercise 7 optional empty assertion test

--- src/test/java/level2/ejercicio7/Exercise7Test.java
+++ src/test/java/level2/ejercicio7/Exercise7Test.java
@@ -1,4 +1,18 @@
 package level2.ejercicio7;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Exercise7Test {
+
+    @Test
+    void shouldVerifyOptionalIsEmpty() {
+
+        Optional<String> optional = Optional.empty();
+
+        assertThat(optional).isEmpty();
+    }
 }
