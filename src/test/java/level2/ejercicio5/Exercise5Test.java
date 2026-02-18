b6aafa5089f0e94cafe82192c734caaa73ced632
David Toledo <toledo_1990@hotmail.com>
2026-02-18 13:53:02 +0100

    feat: add AssertJ exercise 5 map key assertion test

--- src/test/java/level2/ejercicio5/Exercise5Test.java
+++ src/test/java/level2/ejercicio5/Exercise5Test.java
@@ -1,4 +1,23 @@
 package level2.ejercicio5;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Exercise5Test {
+
+    @Test
+    void shouldVerifyMapContainsKey() {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("apple", 1);
+        map.put("banana", 2);
+        map.put("orange", 3);
+
+        assertThat(map).containsKey("banana");
+    }
 }
